28ab6842067c5f4436857271ac86aac52e96be0f
Elliot <elliotholbrow@gmail.com>
2019-07-04 16:38:12 +0100

    update test_boggle.py

--- test_boggle.py
+++ test_boggle.py
@@ -2,38 +2,35 @@ import unittest
 import boggle
 from string import ascii_uppercase
 
-class test_boggle(unittest.TestCase):
+
+class TestBoggle(unittest.TestCase):
     """
     Our test suite for boggle solver
     """
+
     def test_can_create_an_empty_grid(self):
         """
         Test to see if we can create an empty grid
         """
-        grid = boggle.make_grid(0,0)
-        self.assertEqual(len(grid),0)
-
-    def test_grid_size_is_width_times_height(self):
+        grid = boggle.make_grid(0, 0)
+        self.assertEqual(len(grid), 0)
+    
+    def test_grid_size_width_times_height(self):
         """
-        Test is to ensure that the total size of the
-        grid is equal to width * height
+        Test is to ensure that the total size of the grid
+        is equal to width * height
         """
         grid = boggle.make_grid(2, 3)
         self.assertEqual(len(grid), 6)
-
+    
     def test_grid_coordinates(self):
-        """
-        Test to ensure that all of the coordinates
-        inside of the grid can be accessed
-        """
-
         grid = boggle.make_grid(2, 2)
         self.assertIn((0, 0), grid)
         self.assertIn((0, 1), grid)
         self.assertIn((1, 0), grid)
         self.assertIn((1, 1), grid)
         self.assertNotIn((2, 2), grid)
-
+    
     def test_grid_is_filled_with_letters(self):
         """
         Ensure that each of the coordinates in the grid
@@ -42,12 +39,11 @@ class test_boggle(unittest.TestCase):
         grid = boggle.make_grid(2, 3)
         for letter in grid.values():
             self.assertIn(letter, ascii_uppercase)
-
+    
     def test_neighbours_of_a_position(self):
         """
         Ensure that a position has 8 neighbours
         """
-
         coords = (1, 2)
         neighbours = boggle.neighbours_of_position(coords)
         self.assertIn((0, 1), neighbours)
@@ -59,6 +55,7 @@ class test_boggle(unittest.TestCase):
         self.assertIn((2, 2), neighbours)
         self.assertIn((2, 3), neighbours)
     
+
     def test_all_grid_neighbours(self):
         """
         Ensure that all of the grid positions have neighbours
@@ -70,7 +67,7 @@ class test_boggle(unittest.TestCase):
             others = list(grid) # create a new list from the dictionary's keys
             others.remove(pos)
             self.assertListEqual(sorted(neighbours[pos]), sorted(others))
-
+    
     def test_converting_a_path_to_a_word(self):
         """
         Ensure that paths can be converted to words
